7e2f86fb17cbe22eb89d6feee648c4c4b023e825
cxjpxl <81886404@qq.com>
2017-05-03 21:32:10 +0800

    修改bug

--- android/src/main/java/com/cxj/react_native_update_android/DownPackage.java
+++ android/src/main/java/com/cxj/react_native_update_android/DownPackage.java
@@ -27,7 +27,7 @@ public class DownPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        modules.add(new DownModule(reactContext));
+        modules.add(new DownModule(reactContext,this.isZw));
         return modules;
     }
 
